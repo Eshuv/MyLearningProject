a80588126dd9bf10d995e0491235ae05699706fb
Abhay <gautameshuv@gmail.com>
2025-07-20 01:41:35 +0530

    New changes

--- src/_2DArray.java
+++ src/_2DArray.java
@@ -4,11 +4,9 @@ public class _2DArray {
     {
         int[][] myNumber = {{1,2,3}, {4,5,6}};
 
-        for(int i = 0; i < myNumber.length; i++)
-        {
-            for(int j = 0 ; j < myNumber[i].length; j++)
-            {
-                System.out.print(myNumber[i][j] + " ");
+        for (int[] myNumber1 : myNumber) {
+            for (int j = 0; j < myNumber1.length; j++) {
+                System.out.print(myNumber1[j] + " ");
             }
         }
 
